77bb909c890b0f1c03915a6e2203471fc7aa901a
itsmeumaima <umaimarauf2005@gmail.com>
2025-01-15 18:56:22 +0500

    Added

---
+++ datatypes.js
@@ -0,0 +1,21 @@
+"use strict" //to treat all the js code as newer version
+
+// alert(3) // get an error because we use nodejs as our compiler, this will run on browser
+
+let st_name="Umaima"; //string
+let id=10;            //number(integer)
+let enrolled=true;    //boolean
+
+// Datatypes
+// 1. number --> any integer
+// 2. bigint --> big numbers
+// 3. string 
+// 4. boolean --> True/False
+// 5. null -->stand alone value
+// 6. undefined --> whose value is not define
+// 7. symbol -->to find the uniqueness
+// 8. object -->non-primitive datatype
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+console.log(typeof id)  //number 
