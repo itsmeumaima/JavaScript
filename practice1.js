ed9e7b3d06eaea40857e849c632400f85a54360c
itsmeumaima <umaimarauf2005@gmail.com>
2025-01-15 17:57:08 +0500

    Added

---
+++ practice1.js
@@ -0,0 +1,15 @@
+// Variable
+// To declare the constant variable
+const id=20;
+
+// We can declare the variable using two keyword
+// 1. let (currently used)
+// 2. var (not used now because of the functional scope problem)
+
+let Student_name="Umaima"
+let age=18
+let dept="Software Engineering"
+
+// To print on the screen
+console.log(Student_name);
+console.table([id,Student_name,age,dept])
